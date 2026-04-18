f27cace5f51924d8190bce5f4fb74f2e8f790d39
brunogoncalves <brunoomiguellg@gmail.com>
2026-04-18 13:06:01 +0100

    + restantes ficheiros projeto base

---
+++ ContaGotas/ContaGotas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContaGotas;
+public class Program
+{
+    Controller controller = new Controller();
+    static void Main(string[] args) {
+        Program app = new Program();
+        // Iniciar o programa
+        app.controller.iniciarAplicacao();
+    }
+}
