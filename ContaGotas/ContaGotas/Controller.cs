49ff254903a04ccaf22903dc56bf4657aa63ca98
Pedro Fernandes <pedrofernandes.geral@gmail.com>
2026-04-25 23:30:07 +0100

    adiciao try-catch e async Task para tratamento de error em OpcaoSelecionada()

--- ContaGotas/ContaGotas/Controller.cs
+++ ContaGotas/ContaGotas/Controller.cs
@@ -35,34 +35,43 @@ public class Controller
         Environment.Exit(0);
     }
 
-    public async void OpcaoSelecionada(int opcao)
+    // Mudei de "async void" para "async Task" e adicionamos try-catch
+    // Quando ligar a API, e a internet for abaixo durante o await model.AtualizarMedias()
+    // o programa vai "engolir" o erro e crashar sem aviso, porque o async void não permite que o erro suba para ser tratado.
+    public async Task OpcaoSelecionada(int opcao)
     {
-        switch (opcao)
-        {
-            case 0:
-                TerminarAplicacao();
-                break;
-            
-            case 1: // Ver médias
-                await model.AtualizarMedias(); //
-                break;
+        try{
+            switch (opcao)
+            {
+                case 0:
+                    TerminarAplicacao();
+                    break;
+                
+                case 1: // Ver médias
+                    await model.AtualizarMedias(); //
+                    break;
 
-            case 2: // Pesquisar distrital
-                //view.atualizarInterface();
-                model.ObterTiposDeCombustivel();
-                //model.ObterDistritos();
-                // model.PesquisaDistrital(distrito, id);
-                break;
-            
-            case 3: //Estatisticas
-                //view.atualizarInterface();
-                model.ObterTiposDeCombustivel();
-                //model.ObterDistritos();
-                break;
-            
-            default:
-                Console.WriteLine("Opção inválida");
-                break;
+                case 2: // Pesquisar distrital
+                    //view.atualizarInterface();
+                    model.ObterTiposDeCombustivel();
+                    //model.ObterDistritos();
+                    // model.PesquisaDistrital(distrito, id);
+                    break;
+                
+                case 3: //Estatisticas
+                    //view.atualizarInterface();
+                    model.ObterTiposDeCombustivel();
+                    //model.ObterDistritos();
+                    break;
+                
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }catch (Exception ex)
+        {
+            // O tratamento de error
+            Console.WriteLine($"\nFalha ao comunicar com o serviço. Detalhes: {ex.Message}");
         }
     }
     
