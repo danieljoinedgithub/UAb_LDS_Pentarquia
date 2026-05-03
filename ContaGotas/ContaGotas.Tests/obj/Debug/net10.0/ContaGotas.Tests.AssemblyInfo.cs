ac7bfef71b057c0ec70a639f462b2bb1d997f87b
brunogoncalves <brunoomiguellg@gmail.com>
2026-05-03 10:26:37 +0100

    + files rebase

--- ContaGotas/ContaGotas.Tests/obj/Debug/net10.0/ContaGotas.Tests.AssemblyInfo.cs
+++ ContaGotas/ContaGotas.Tests/obj/Debug/net10.0/ContaGotas.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec2499d565dcc941544dd34d35740281083066f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49446785be134287088705e3912338e16a1c328f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ContaGotas/ContaGotas/obj/Debug/net10.0/ContaGotas.AssemblyInfo.cs
+++ ContaGotas/ContaGotas/obj/Debug/net10.0/ContaGotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaGotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec2499d565dcc941544dd34d35740281083066f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49446785be134287088705e3912338e16a1c328f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaGotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaGotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
