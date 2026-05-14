f985935502ad6094e57838aa87a25671e1062038
brunogoncalves <brunoomiguellg@gmail.com>
2026-05-14 22:47:37 +0100

    + Novo evento entre view-model para notificação de erros e apresentação. + model com parametros de atualizar médias

--- ContaGotas/ContaGotas.Tests/obj/Debug/net10.0/ContaGotas.Tests.AssemblyInfo.cs
+++ ContaGotas/ContaGotas.Tests/obj/Debug/net10.0/ContaGotas.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569330c4099cc9e2ce4e7023b522ef366c2c0d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10eb6062c9d594a1db35952829e03bd38d711b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaGotas.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
