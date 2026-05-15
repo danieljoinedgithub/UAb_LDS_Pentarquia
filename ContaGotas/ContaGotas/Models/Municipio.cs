0906faeecd4a0c8573d6f2c3e84217c04fbfdd13
BlackstonePT <Diogoalexsoares@sapo.pt>
2026-05-15 16:29:13 +0000

    Separaçao de classes para ficheiros separados

---
+++ ContaGotas/ContaGotas/Models/Municipio.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+
+namespace ContaGotas;
+
+public class Municipio : IValido
+{ 
+    [JsonProperty("ID")]
+    public int Id { get; set; } 
+    
+    [JsonProperty("Descricao")]
+    public string Nome { get; set; } 
+    
+    public bool IsValido()
+    {
+        //Validação ID
+        if (Id < 0 )
+            return false;
+        //Validação do Nome
+        if (string.IsNullOrWhiteSpace(Nome))
+            return false;
+
+        return true;
+    }
+}
