bbec21eb11a7db7969016a9ca6eb42d4150f0158
BlackstonePT <Diogoalexsoares@sapo.pt>
2026-06-05 14:05:48 +0000

    adicionar funcionalidade de abrir mapa para GUI Pesquisa Distrital

--- ContaGotas/ContaGotas/Models/PostoModel.cs
+++ ContaGotas/ContaGotas/Models/PostoModel.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Newtonsoft.Json;
 
 namespace ContaGotas;
@@ -5,32 +6,43 @@ namespace ContaGotas;
 public class PostoModel : IValido
 { 
     [JsonProperty("Id", Required = Required.Always)]
-    public int  Id { get; set; } //required para obrigar a preencer com valor
+    public int  id { get; set; } //required para obrigar a preencer com valor
     
     [JsonProperty("Nome")]
-    public string Nome { get; set; } 
+    public string nome { get; set; } 
     
     [JsonProperty("Morada")]
-    public string Morada { get; set; }
+    public string morada { get; set; }
     
     [JsonProperty("Preco", Required = Required.Always)]
-    public decimal Preco { get; set; } // A API às vezes envia como string "1,749 €"
+    public decimal preco { get; set; } // A API às vezes envia como string "1,749 €"
+    
+    [JsonProperty("Latitude")]
+    public double latitude { get; set;}
+   
+    [JsonProperty("Longitude")]
+    public double longitude { get; set; }
     
     public bool IsValido()
     {
         //Validação ID
-        if (Id < 0 )
+        if (id < 0 )
             return false;
         //Validação do Nome
-        if (string.IsNullOrWhiteSpace(Nome))
+        if (string.IsNullOrWhiteSpace(nome))
             return false;
-        if (string.IsNullOrWhiteSpace(Morada))
+        if (string.IsNullOrWhiteSpace(morada))
             return false;
-        if (Preco <= 0)
+        if (preco <= 0)
             return false;
 
         return true;
     }
+    //para o float não vir com ',' para abrir o mapa 
+    [JsonIgnore] 
+    public string LatitudeToString => latitude.ToString(CultureInfo.InvariantCulture);
+    [JsonIgnore] 
+    public string LongitudeToString => longitude.ToString(CultureInfo.InvariantCulture);
 
     // Propriedade auxiliar para cálculos
     //public double Preco => double.TryParse(PrecoString?.Split(' ')[0].Replace(',', '.'), out var res) ? res : 0;
